389a9d5ca716a96892746111478ec7205041c87a
Danny Milsom <danny.milsom@cgi.com>
2014-07-25 14:56:12 +0000

    Fixes #4715 - Creating a project from a template now imports all data previously exported into XML
    * Once validated we add the full filepath to the individual template directory to the options dictionary in manage_project.py
    * This means only need to pass this filepath to the methods inside the ImportTemplate class (not the name too)
    * In importer.py we now more intelligently join the filepath passed as an argument to different xml file names
    * We also get the repository path via the repository_dir trac.ini option rather than naively hacking together a path
    * In the future we should probably use ResourceManager to improve this further - but I couldn't get it to work
    * My manual testing showed that it now imports:
      * wiki pages
      * wiki attachments
      * permissions
      * groups
      * milestones
      * components
      * priorities
      * ticket types
      * custom workflows
      * mailing lists
      * file archve (Subversion only)
    * I plan to add some unit tests to this plugin soon to demonstrate this
    
    git-svn-id: file:///mnt/ramdisk/d4-svn/trunk@24253 484df14f-75d2-4b3f-8ba0-490e05069a01

--- createtemplate/importer.py
+++ createtemplate/importer.py
@@ -27,7 +27,7 @@ class ImportTemplate(Component):
     template_dir_path = PathOption('project_templates', 'template_dir', 
                         doc="The default path for the project template directory")
 
-    def import_wiki_pages(self, template_name):
+    def import_wiki_pages(self, template_path):
         """Creates wiki pages from wiki.xml template file.
 
         Creates wiki pages inside the project using data extracted from
@@ -36,7 +36,7 @@ class ImportTemplate(Component):
         """
 
         # open the wiki XML file, parse the data and create wiki pages
-        full_path = os.path.join('templates', template_name, 'wiki.xml')
+        full_path = os.path.join(template_path, 'wiki.xml')
         try:
             tree = ET.ElementTree(file=full_path)
             for page in tree.getroot():
@@ -50,7 +50,7 @@ class ImportTemplate(Component):
                 self.log.info("Path to wiki.xml file %s does not exist. Unable "
                               "to import wiki pages from template.", full_path)
 
-    def import_wiki_attachments(self, template_name, project_name):
+    def import_wiki_attachments(self, template_path):
         """Imports wiki attachments from template.
 
         Imports wiki attachment files and inserts associated data into 
@@ -58,7 +58,7 @@ class ImportTemplate(Component):
         """
 
         # check that there are attachments to import
-        template_attachment_path = os.path.join('templates', template_name, 'attachments', 'wiki')
+        template_attachment_path = os.path.join(template_path, 'attachments', 'wiki')
         if os.path.isdir(template_attachment_path):
 
             # the directory we copy to can't exist before the copytree() so we delete it just incase
@@ -89,7 +89,7 @@ class ImportTemplate(Component):
                 cursor = db.cursor()
                 cursor.execute("DELETE FROM attachment WHERE type='wiki'")
 
-                filepath = os.path.join('templates', template_name, 'attachment.xml')
+                filepath = os.path.join(template_path, 'attachment.xml')
                 tree = ET.ElementTree(file=filepath)
                 attachment_info = [('wiki', att.attrib['parent_id'], att.attrib['name'], 
                                     att.attrib['version'], att.attrib['size'], att.text)
@@ -98,7 +98,7 @@ class ImportTemplate(Component):
                 cursor.executemany("""INSERT INTO attachment(type, id, filename, version, size, description)
                                       VALUES (%s, %s, %s, %s, %s, %s)""", attachment_info)
 
-    def template_populate(self, template_name, project_name):
+    def template_populate(self, template_path):
         """Clears default data and inserts template specific data from xml files.
 
         Clears tables of define/trac default data and repopulates them with 
@@ -116,33 +116,33 @@ class ImportTemplate(Component):
         enum_to_clear = list()
 
         # go through template dir to see which tables and rows we want to modify
-        dir_path = os.path.join('templates', template_name)
+        dir_path = os.path.join(template_path)
         try:
             for filename in os.listdir(dir_path):
                 if filename.lower().endswith("permission.xml"):
-                    self.import_perms(template_name)
+                    self.import_perms(template_path)
                 elif filename.lower().endswith("group.xml"):
-                    self.import_groups(template_name)
+                    self.import_groups(template_path)
                 elif filename.lower().endswith("milestone.xml"):
-                    self.import_milestones(template_name)
+                    self.import_milestones(template_path)
                 # we don't import version right now as this
                 # raises a bug when we import ticket types
                 #elif filename.lower().endswith("version.xml"):
-                    #self.import_versions(template_name)
+                    #self.import_versions(template_path)
                 elif filename.lower().endswith("component.xml"):
-                    self.import_components(template_name)
+                    self.import_components(template_path)
                 elif filename.lower().endswith("priority.xml"):
                     enum_to_clear.append("priority")
                 elif filename.lower().endswith("ticket.xml"):
                     enum_to_clear.append("ticket_type")
             if enum_to_clear:
-                self.import_enum(template_name, enum_to_clear, project_name)
+                self.import_enum(template_path, enum_to_clear)
         except OSError as exception:
             if exception.errno == errno.ENOENT:
                 self.log.info("Unable to list files at %s."
                               "Import of template data failed.", dir_path)
 
-    def import_perms(self, template_name):
+    def import_perms(self, template_path):
         """Creates permissions from data stored in permission.xml.
 
         Parses the permissions XML file to get the data we need to insert
@@ -155,7 +155,7 @@ class ImportTemplate(Component):
         """
 
         # parse the tree to get username, action data
-        path = os.path.join('templates', template_name, "permission.xml")
+        path = os.path.join(template_path, "permission.xml")
         try:
             tree = ET.ElementTree(file=path)
             perm_data = [(perm.attrib['name'], perm.attrib['action']) for perm in tree.getroot()]
@@ -178,7 +178,7 @@ class ImportTemplate(Component):
             cursor.executemany("""INSERT INTO permission(username, action)
                                   VALUES (%s, %s)""", perm_data)
 
-    def import_groups(self, template_name):
+    def import_groups(self, template_path):
         """Create project groups from group.xml template file.
 
         First we clear the existing data in the groups table and then we insert
@@ -194,7 +194,7 @@ class ImportTemplate(Component):
             cursor.execute("DELETE FROM groups")
 
         self.log.info("Creating groups from template")
-        path = os.path.join('templates', template_name, "group.xml")
+        path = os.path.join(template_path, "group.xml")
         try:
             tree = ET.ElementTree(file=path)
             for group in tree.getroot():
@@ -205,7 +205,7 @@ class ImportTemplate(Component):
                 self.log.info("Path to group.xml at %s does not exist. Unable to "
                               "import group data from template.", path)
 
-    def import_milestones(self, template_name):
+    def import_milestones(self, template_path):
         """Create project milestones from milestone.xml template file.
 
         Deletes existing trac default milestones and creates new ones
@@ -221,7 +221,7 @@ class ImportTemplate(Component):
             cursor.execute("""DELETE FROM milestone""")
 
         # Parse the XML tree and create milestones
-        path = os.path.join('templates', template_name, "milestone.xml")
+        path = os.path.join(template_path, "milestone.xml")
         try:
             tree = ET.ElementTree(file=path)
             for m in tree.getroot():
@@ -245,7 +245,7 @@ class ImportTemplate(Component):
                 self.log.info("Path to milestone.xml at %s does not exist. "
                               "Unable to import milestone data from tempalte.", path)
 
-    def import_versions(self, template_name):
+    def import_versions(self, template_path):
         """Create project milestones from milestone.xml template file.
 
         Create ticket verions from template after clearing the existing
@@ -262,7 +262,7 @@ class ImportTemplate(Component):
             cursor.execute("DELETE FROM version")
 
         self.log.info("Creating versions from template")
-        path = os.path.join('templates', template_name, "version.xml")
+        path = os.path.join(template_path, "version.xml")
         try:
             tree = ET.ElementTree(file=path)
             for version in tree.getroot():
@@ -275,7 +275,7 @@ class ImportTemplate(Component):
                 self.log.info("Path to version.xml at %s does not exist. Unable to "
                               "import version data from template.", path)
 
-    def import_components(self, template_name):
+    def import_components(self, template_path):
         """Create project components from component.xml template file.
 
         Create project component fields from template after clearing the 
@@ -292,7 +292,7 @@ class ImportTemplate(Component):
             cursor.execute("DELETE FROM component")
 
         self.log.info("Creating components from template")
-        path = os.path.join('templates', template_name, "component.xml")
+        path = os.path.join(template_path, "component.xml")
         try:
             tree = ET.ElementTree(file=path)
             for component in tree.getroot():
@@ -307,13 +307,12 @@ class ImportTemplate(Component):
                 self.log.info("Path to component.xml at %s does not exist. Unable to "
                               "import component data from template.", path)
 
-    def import_enum(self, template_name, types_to_remove, project_name):
+    def import_enum(self, template_path, types_to_remove):
         """Removes types from the enum table and then inserts data from the 
         template XML files."""
 
         # create a list of tuples for every enum type in our template 
         # where the tuple follows the synax (type, name, value)
-        template_path = os.path.join('templates', template_name)
         path = os.path.join(template_path, 'priority.xml')
         try:
             tree = ET.ElementTree(file=path)
@@ -370,14 +369,14 @@ class ImportTemplate(Component):
                 self.log.info("Path to ticket.xml at %s does not exist. "
                               "Unable to import tickets from tempalte.", path)
 
-    def import_workflows(self, template_name):
+    def import_workflows(self, template_path):
         """Imports workflows from template workflow directory.
 
         Copies all workflow files from the template directory to our new 
         project's workflow directory.
         """
 
-        template_workflow_path = os.path.join(template_name, 'workflows')
+        template_workflow_path = os.path.join(template_path, 'workflows')
         project_workflow_path = os.path.join(self.env.path, 'workflows')
 
         # the directory we copy to can't exist if using shutil.copytree
@@ -395,10 +394,10 @@ class ImportTemplate(Component):
                 self.log.info("The path to the workflow directory at %s does "
                               "not exist. Unable to import workflows.", template_workflow_path)
 
-    def import_mailinglist(self, template_name):
+    def import_mailinglist(self, template_path):
         """Creates project mailing lists from list.xml template file."""
 
-        path = os.path.join('templates', template_name, 'list.xml')
+        path = os.path.join(template_path, 'list.xml')
         try:
             tree = ET.ElementTree(file=path)
             for ml in tree.getroot():
@@ -417,15 +416,18 @@ class ImportTemplate(Component):
         # TODO Get Subscriber informaiton 
         # mailinglist.subscribe(group='project_group', poser=True)
 
-    def import_file_archive(self, template_name, project_name):
+    def import_file_archive(self, template_path):
         """Import the file archive from template directory.
         
         Create a new subversion repository using the dump file in 
         the template directory. We don't support Git right now.
         """
 
-        old_repo_path = os.path.join('templates', template_name,  template_name + '.dump.gz')
-        new_repo_path = os.path.join('vc-repos', 'svn', project_name)
+        template_name = os.path.basename(os.path.normpath(template_path))
+        old_repo_path = os.path.join(template_path,  template_name + '.dump.gz')
+
+        # should probably use ResourceManager from trac/versioncontrol...
+        new_repo_path = self.env.config.get('trac', 'repository_dir')
 
         subprocess.call("zcat %s | svnadmin load %s" % (old_repo_path, new_repo_path), cwd=os.getcwd(), shell=True)
         self.log.info("Imported Subversion file archive from %s" % old_repo_path)
